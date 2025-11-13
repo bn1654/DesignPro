312815ac62a94b63bc35d4f48740b7b8208daec7
bn1654 <stikenloni@gmail.com>
2025-11-13 10:10:46 +0700

    dafault page and index page created

--- designsite/designsite/urls.py
+++ designsite/designsite/urls.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('requests/', include('requests.urls')),
+    path('', RedirectView.as_view(url='/requests/')),
+    path('catalog/', RedirectView.as_view(url='/requests/')), #Почему то он пытается зайти на url прошлого задания (кеш браузера чистил раза 4)
+
+    
 ]
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
