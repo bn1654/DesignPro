2e7b6ed336293f32544a731ccf725a82e8cfd14c
bn1654 <stikenloni@gmail.com>
2025-11-14 12:29:09 +0700

    main page

--- designsite/requests/views.py
+++ designsite/requests/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
+from .models import Request
 
 
 
 def index(request):
+    
+    request_list = Request.objects.filter(status='c')
+    requests_is_accepted = Request.objects.filter(status='a').count()
+    print(requests_is_accepted)
+    
     return render(
     request,
     'requests/index.html',
-     context={},
+    context={'request_list': request_list, 'requests_is_accepted':requests_is_accepted,},
 )
+
