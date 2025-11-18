e9daf9d2de00c50d3244a4595e12d02778731648
bn1654 <stikenloni@gmail.com>
2025-11-18 09:28:08 +0700

    reworked forms

--- requests/forms.py
+++ requests/forms.py
@@ -1,16 +1,49 @@
+import re
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import AbsUser
-from . import validators
+from django.core.exceptions import ValidationError
 
 
 class SignUpForm(UserCreationForm):
-    FIO = forms.CharField(max_length=255, required=True, help_text='Ваше ФИО', validators=[validators.validate_FIO], label='ФИО')
-    username = forms.CharField(label='Логин')
+    FIO = forms.CharField(required=True, help_text='Ваше ФИО, только кириллица, пробелы и дефис', label='ФИО')
+    username = forms.CharField(label='Логин', help_text='Ваш логин, только латинница и дефис')
+    
+    privacy_agreement = forms.BooleanField(label="Согласие на обработку персональных данных", help_text="Для регистрации необходимо дать свое согласие", required=True)
+    
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        
+        if AbsUser.objects.filter(username=username).exists():
+            raise ValidationError("Пользователь с таким логином уже существует")
+        
+        if not re.match(r'^[a-z\-]+$', username.lower()):
+            raise ValidationError("Логин введен неверно")
+        
+        return username
+    
+    def clean_FIO(self):
+        FIO = self.cleaned_data['FIO']
+            
+        if not re.match(r'^[а-я\-\s]+$', FIO.lower()):
+            print(re.match(r'^[а-яё\-\s]+$', FIO.lower()))
+            raise ValidationError("ФИО введены неверно")
+
+        return FIO
+        
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        
+        # Проверка на уникальность
+        if AbsUser.objects.filter(email=email).exists():
+            raise ValidationError("Пользователь с таким email уже существует")
+        
+        return email
     
     class Meta:
         model = AbsUser
-        fields = ('FIO', 'username', 'email', 'password1', 'password2',)
+        fields = ('FIO', 'username', 'email', 'password1', 'password2', 'privacy_agreement')
+
 
 class LoginForm(AuthenticationForm):
-    username = forms.CharField(label='Логин или email')
+    username = forms.CharField(label='Логин')
--- requests/models.py
+++ requests/models.py
@@ -1,27 +1,30 @@
 from django.db import models
-from . import validators
 from datetime import datetime, timedelta
 from .utilities import rename_image
 from django.contrib.auth.models import AbstractUser
 
 class AbsUser(AbstractUser):
-    FIO = models.CharField(verbose_name="ФИО", max_length=255)
-    email=models.EmailField(verbose_name="email", max_length=255, unique=True)
+    FIO = models.CharField(verbose_name="ФИО")
+    email=models.EmailField(verbose_name="email", unique=True)
+    
+    def __str__(self):
+        return self.username
+    
     
     class Meta(AbstractUser.Meta):
         pass
 
 class Category(models.Model):
-    name  = models.CharField(verbose_name="name", max_length=200, help_text="name of category")
+    name  = models.CharField(verbose_name="Название", help_text="Название категории")
     
     def __str__(self):
         return self.name
 
 class Request(models.Model):
-    name = models.CharField(verbose_name="name", max_length=200, help_text="name of design")
-    summary = models.TextField(verbose_name="summary", max_length=500, help_text="design's summary")
-    image = models.ImageField(verbose_name="image",null=True,blank=True,help_text="design's image",upload_to=rename_image)
-    creation_date = models.DateField(verbose_name="creation date", default=datetime.now(), help_text="date and time of design's creation")
+    name = models.CharField(verbose_name="Название", help_text="Название заявки")
+    summary = models.TextField(verbose_name="Описание", help_text="Описание заявки")
+    image = models.ImageField(verbose_name="Изображение",null=True,blank=True,help_text="Изображение дизайна",upload_to=rename_image)
+    creation_date = models.DateField(verbose_name="Дата создания", default=datetime.now(), help_text="Дата и время создания заявки")
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     
     REQUEST_STATUS = [
